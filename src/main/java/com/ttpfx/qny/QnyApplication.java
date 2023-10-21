087c43b888e81188c177b709c09292cb6d27f90d
ttpfx <ttpfx123@gmail.com>
2023-10-21 19:03:49 +0800

    修改项目结构

---
+++ src/main/java/com/ttpfx/qny/QnyApplication.java
@@ -0,0 +1,13 @@
+package com.ttpfx.qny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnyApplication.class, args);
+    }
+
+}
