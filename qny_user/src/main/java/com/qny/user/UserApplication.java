29c02df3d1d7372df8da1a4bed57351da17de962
ttpfx <ttpfx123@gmail.com>
2023-10-24 19:15:13 +0800

    项目结构重建

---
+++ qny_user/src/main/java/com/qny/user/UserApplication.java
@@ -0,0 +1,16 @@
+package com.qny.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ttpfx
+ * @since 2023/10/24
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
--- qny_video/src/main/java/com/qny/video/VideoApplication.java
+++ qny_video/src/main/java/com/qny/video/VideoApplication.java
@@ -1,13 +1,16 @@
-package com.ttpfx.qny;
+package com.qny.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ttpfx
+ * @since 2023/10/24
+ */
 @SpringBootApplication
-public class QnyApplication {
+public class VideoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QnyApplication.class, args);
+        SpringApplication.run(VideoApplication.class, args);
     }
-
 }
